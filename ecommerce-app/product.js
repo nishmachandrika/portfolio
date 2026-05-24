dfcfa8f04b338d895ccfea9dff7834b50a7aaa60
nishmachandrika <s210862@rguktsklm.ac.in>
2026-05-24 16:11:00 +0530

    Add files via upload
    ecommerce website

---
+++ ecommerce-app/product.js
@@ -0,0 +1,7 @@
+const products = [
+    { id: 1, name: "Laptop", price: 50000 },
+    { id: 2, name: "Phone", price: 20000 },
+    { id: 3, name: "Headphones", price: 2000 },
+    { id: 4, name: "Watch", price: 3000 },
+    { id: 5, name: "Shoes", price: 2500 }
+];
